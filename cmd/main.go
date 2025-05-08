42a564275c26453e45a4fae8d860e67e2ee17629
chairat chaikong <chairat.ch@tcc-technology.com>
2025-05-08 10:27:16 +0700

    feat: add CORS middleware to the main application

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -27,6 +28,7 @@ func main() {
 	}
 
 	app := fiber.New()
+	app.Use(cors.New())
 
 	routes.InitRoutes(app, db)
 
